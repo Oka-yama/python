5c22568fddbca35a0398fa2baf9a9bc2a841520e
BRYAN OLIVEIRA DE CARVALHO <bryan.carvalho@aluno.senac.rio>
2026-05-08 19:42:01 -0300

    08/05/2026

--- repeticao/Desafio Python/main.py
+++ repeticao/Desafio Python/main.py
@@ -1,24 +1,97 @@
+#Cadastrar candidato ou colaborador
+#Listar todos os candidatos
+#Buscar uma pessoa pelo nome
+#Alterar uma informação cadastrada
+#Remover uma pessoa da lista
+#Exibir um resumo do processo seletivo
+#Sair do sistema
 
-opcoes = ''
-while opcoes != "7":
-      print("\n======================================",
-            "\nOpções:   | Funcionalidades:"
-            "\n--- 1 --- | Cadastrar candidato/colaborador",
-            "\n--- 2 --- | Listar todos os cadastros",
-            "\n--- 3 --- | Buscar uma pessoa pelo nome",
-            "\n--- 4 --- | Alterar uma informação cadastrada",
-            "\n--- 5 --- | Remover uma pessoa da lista",
-            "\n--- 6 --- | Exibir resumo do processo seletivo",
-            "\n--- 7 --- | Sair do sistema")
-      opcoes = input('Selecione uma opção: ')
-      if opcoes > '7':
-          print('Opção inválida')
+status_permitidos = ('Em análise', "Aprovado", "Reprovado", "Contratado")
 
-cadastro = {
-    "Nome": input('Insira seu nome: '),
-    "Idade": input('Insira sua idade: '),
-    "Cargo pretendido": input('Insira o cargo pretendido: '),
-    "Salário desejado": input('Insira o salário desejado: '),
-    "Status do processo": input('Insira o status processo: ')
+pessoas = []
+
+def mostrar_menu():
+    print("--- SISTEMA DE CADASTRO DE CANDIDATOS ---")
+    print(" (Opções: | Funcionalidades:")
+    print("--- 1 --- | Cadastrar candidato/colaborador")
+    print("--- 2 --- | Listar todos os cadastros")
+    print("--- 3 --- | Buscar uma pessoa pelo nome")
+    print("--- 4 --- | Alterar uma informação cadastrada")
+    print("--- 5 --- | Remover uma pessoa da lista")
+    print("--- 6 --- | Exibir resumo do processo seletivo")
+    print("--- 7 --- | Sair do sistema")
+
+def cadastrar_pessoas():
+    print("\n--- Cadastro de candidato/colaborar ---")
+    nome = input("Nome: ").strip()
+    if nome == "":
+        print("O nome não pode ser vazio")
+        return
+
+    for pessoa in pessoas:
+            if pessoa["nome"].lower() == nome.lower():
+                print("Já existe uma pessoa com esse nome")
+                return
+
+    try:
+        idade = int(input("Idade: "))
+        salario = float(input("Salário desejado: "))
+    except:
+        print("Idade e salário precisam ser valores numéricos")
+        return
+
+    cargo = input("Cargo pretendido: ").strip()
+    if cargo == "":
+        print("O cargo não pode ser vazio")
+        return
+
+    print("\n Status permitidos: ")
+    for status in status_permitidos:
+        print(f"-{status}")
+
+    status = input("Status do processo: ").strip()
+
+    if status not in status_permitidos:
+        print("Status inválido.")
+        return
+
+    pessoa = {
+        "nome": nome,
+        "idade": idade,
+        "cargo": cargo,
+        "status": status
     }
-print(cadastro)
+
+    pessoas.append(pessoa)
+    print("Pessoa cadastrada com sucesso!")
+
+def buscar_pessoas():
+    print("\n--- Buscar pessoa pelo nome ---")
+
+    if len(pessoas) == 0:
+        print('Nenhuma pessoa cadastrada')
+        return
+
+    for indice, pessoa in enumerate(pessoas, start=1):
+        print(f"\n Cadastro {indice}")
+        print(f"Nome: {pessoa['nome']}")
+        print(f"Idade: {pessoa['idade']}")
+        print(f"Cargo pretendido: {pessoa['cargo']}")
+        print(f"Salário desejado: {pessoa['salario']:.2f}")
+        print(f"Status: {pessoa['status']}")
+    nome_busca = input("Digite o nome para buscar: ").strip().lower()
+    encontrou = False
+    for pessoa in pessoas:
+        if pessoa["nome"].lower() == nome_busca:
+            print('Pessoa encontrada!')
+            print(f"Nome: {pessoa['nome']}")
+            print(f"Idade: {pessoa['idade']}")
+            print(f"Cargo pretendido:"
+                  f" {pessoa['nome']}")
+            print(f"Salário pretendido: {pessoa['salario']}")
+            print(f"Status: {pessoa['status']}")
+            encontrou = True
+            break
+
+    if not encontrou:
+        print('Pessoa não encontrada!')
