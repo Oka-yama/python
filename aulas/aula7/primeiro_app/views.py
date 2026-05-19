aee5014b8980deb6d6f914118458f578e13a6620
Oka-yama <bryancarvalhonovo@gmail.com>
2026-05-18 21:30:34 -0300

    18/05/26

--- aulas/aula7/primeiro_app/views.py
+++ aulas/aula7/primeiro_app/views.py
@@ -6,4 +6,10 @@ from django.shortcuts import render
 # Create your views here.
 # Define a view function chamada 'home' que recebe um objeto 'request' como argumento e retorna uma resposta Http
 def home(request):
-    return HttpResponse('Olá, este é o meu primeiro projeto Django!')
+    return render(request, 'home.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html')
--- aulas/aula7/primeiro_projeto/urls.py
+++ aulas/aula7/primeiro_projeto/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from primeiro_app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('', include('primeiro_app.urls')),
 ]
